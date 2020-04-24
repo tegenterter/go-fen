036af65144230f40312d3f6004c0424c224187e2
Thibaut Vandenbussche <thibaut.vandenbussche@showpad.com>
2020-04-24 21:35:19 +0200

    Slight refactor

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"bytes"
-	"github.com/gorilla/mux"
+	"fmt"
 	"image"
 	"image/color"
 	"image/draw"
@@ -16,6 +16,35 @@ import (
 )
 
 const dimension = 45
+
+func drawDiagram(fen string) image.Image {
+	// Draw an empty board
+	board := drawBoard(dimension, color.RGBA{209, 139, 71, 255}, color.RGBA{255, 206, 158, 255})
+
+	rgxp := regexp.MustCompile(`(?i)^[a-z\d+\/]+`)
+
+	// Iterate the separate ranks as defined in the FEN string
+	for i, row := range strings.Split(rgxp.FindString(fen), "/") {
+		k := 0
+
+		for j := 0; j < len(row); j++ {
+			character := string(row[j])
+			// Check if the character is numeric
+			emptySquares, err := strconv.Atoi(character)
+
+			if err == nil {
+				// Pad the number of empty squares
+				k += emptySquares
+			} else {
+				// Draw the piece onto the board
+				draw.Draw(board, image.Rect(0, 0, dimension, dimension).Add(image.Point{k * dimension, i * dimension}), getPiece(row[j]), image.Point{}, draw.Over)
+				k++
+			}
+		}
+	}
+
+	return board
+}
 
 func drawBoard(squareDimension int, lightColor color.RGBA, darkColor color.RGBA) *image.RGBA {
 	board := image.NewRGBA(image.Rect(0, 0, squareDimension * 8, squareDimension * 8))
@@ -31,7 +60,10 @@ func drawBoard(squareDimension int, lightColor color.RGBA, darkColor color.RGBA)
 
 		for j := 0; j < 8; j++ {
 			file := j * dimension
+
+			// Draw an individual square
 			draw.Draw(board, image.Rect(rank, file, rank + dimension, file + dimension), &image.Uniform{colors[c]}, image.Point{}, draw.Src)
+
 			c = 1 - c
 		}
 
@@ -39,36 +71,6 @@ func drawBoard(squareDimension int, lightColor color.RGBA, darkColor color.RGBA)
 	}
 
 	return board
-}
-
-func diagram(w http.ResponseWriter, r *http.Request) {
-	fen := mux.Vars(r)["fen"]
-
-	board := drawBoard(dimension, color.RGBA{209, 139, 71, 255}, color.RGBA{255, 206, 158, 255})
-	rgxp := regexp.MustCompile(`(?i)^[a-z\d+\/]+`)
-
-	for i, row := range strings.Split(rgxp.FindString(fen), "/") {
-		k := 0
-
-		for j := 0; j < len(row); j++ {
-			character := string(row[j])
-			emptySquares, err := strconv.Atoi(character)
-
-			if err == nil {
-				k += emptySquares
-			} else {
-				draw.Draw(board, image.Rect(0, 0, dimension, dimension).Add(image.Point{k * dimension, dimension * i}), getPiece(row[j]), image.Point{}, draw.Over)
-				k++
-			}
-		}
-	}
-
-	buffer := new(bytes.Buffer)
-	png.Encode(buffer, board)
-
-	w.Header().Set("Content-Type", "image/png")
-	w.Header().Set("Content-Length", strconv.Itoa(len(buffer.Bytes())))
-	w.Write(buffer.Bytes())
 }
 
 func getPiece(character uint8) image.Image {
@@ -90,7 +92,27 @@ func getPiece(character uint8) image.Image {
 }
 
 func main() {
-	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/{fen:.*}", diagram)
-	http.ListenAndServe(":8080", router)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fen := r.URL.Path[1:]
+		rgxp := regexp.MustCompile(`(?i)^[rnbqk1-7]{1,8}\/[rnbqkp1-7]{1,8}\/[rnbqkp1-7]{1,8}\/[rnbqkp1-7]{1,8}\/[rnbqkp1-7]{1,8}\/[rnbqkp1-7]{1,8}\/[rnbqkp1-7]{1,8}\/[rnbqk1-7]{1,8}\sw|b\sk|q|-\s\d+\s\d+$`)
+
+		if rgxp.MatchString(fen) == false {
+			http.Error(w, "Invalid FEN string", http.StatusBadRequest)
+
+			fmt.Println("GET", http.StatusBadRequest, "/" + fen)
+			return
+		}
+
+		buffer := new(bytes.Buffer)
+		// Generate the diagram
+		png.Encode(buffer, drawDiagram(fen))
+
+		fmt.Println("GET", http.StatusOK, "/" + fen)
+
+		w.Header().Set("Content-Type", "image/png")
+		w.Header().Set("Content-Length", strconv.Itoa(len(buffer.Bytes())))
+		w.Write(buffer.Bytes())
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
